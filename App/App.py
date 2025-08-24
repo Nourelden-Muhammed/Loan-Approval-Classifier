bbee7689a54b83370aa1c60a3dab063bd9e39455
Nourelden Hany Muhammed <nour.hany.elhakiem@gmail.com>
2025-08-24 08:30:54 +0300

    Update App.py

--- App/App.py
+++ App/App.py
@@ -268,7 +268,7 @@ def main():
         )
     
     # Load selected model
-    model_path = "models/best_random_forest_model.pkl" if model_option == "Random Forest" else "models/best_xgboost_model.pkl"
+    model_path = "Models/best_random_forest_model.pkl" if model_option == "Random Forest" else "Models/best_xgboost_model.pkl"
     
     try:
         model, model_features = load_model(model_path)
@@ -527,5 +527,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
